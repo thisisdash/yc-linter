711b0cb5715f4211576d601007880435991c7ba7
pietvanzoen <hi@pietvanzoen.com>
2019-01-23 10:49:40 +0100

    feat: add prettier formatter to yc-base

--- packages/eslint-config-yc-base/index.js
+++ packages/eslint-config-yc-base/index.js
@@ -1,14 +1,14 @@
 module.exports = {
   env: {
-    'es6': true,
-    'jest/globals': true
+    es6: true,
+    "jest/globals": true
   },
   extends: [
-    'eslint-config-airbnb-base',
-    'eslint-config-airbnb-base/rules/strict',
-    './rules/rules'
+    "eslint-config-airbnb-base",
+    "eslint-config-airbnb-base/rules/strict",
+    "./rules/rules"
   ]
     .map(require.resolve)
-    .concat(['plugin:jest/recommended']),
-  plugins: ['jest']
+    .concat(["plugin:jest/recommended"]),
+  plugins: ["jest", "prettier"]
 };
